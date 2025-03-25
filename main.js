d4581e29066b03e551b8907b4f4a161443c9c991
doomovski <mega-vl2004@mail.ru>
2025-03-25 21:23:10 +1000

    внешка закончена

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const leftButton = document.querySelector('.get-left');
 const rightButton = document.querySelector('.get-right'); 
 const row = document.querySelector('.row');
 const testimonials = document.querySelectorAll('.testimonial');
+const likeButtons = document.querySelectorAll('.like-button'); 
 const testimonialWidth = testimonials[0].offsetWidth + 20; 
 let currentPosition = 0;
 const maxPosition = -(testimonials.length - 2) * testimonialWidth;
@@ -18,4 +19,10 @@ rightButton.addEventListener('click', () => {
         currentPosition += testimonialWidth;
         row.style.transform = `translateX(${currentPosition}px)`;
     }
+});
+
+likeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.toggle('inverted'); 
+    });
 });
