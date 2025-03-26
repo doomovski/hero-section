f39aef8a6987ebdfdb896cb748bf96a0556b7a0e
doomovski <mega-vl2004@mail.ru>
2025-03-26 19:01:38 +1000

    Реально конец, всё!!!

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const burger = document.querySelector('.burger');
 const navMenu = document.querySelector('nav ul');
 const headerButtons = document.querySelector('.header-button');
 
+// Обозначение сдвига
 let testimonialWidth = testimonials.length > 0 ? testimonials[0].offsetWidth + 44 : 0;
 let currentPosition = 0;
 const maxPosition = -(testimonials.length - 2) * testimonialWidth;
@@ -14,7 +15,7 @@ const maxPosition = -(testimonials.length - 2) * testimonialWidth;
 window.addEventListener('resize', () => {
     testimonialWidth = testimonials.length > 0 ? testimonials[0].offsetWidth + 20 : 0;
 });
-
+//Функционал кнопок лево\права
 leftButton.addEventListener('click', () => {
     if (currentPosition > maxPosition) {
         currentPosition -= testimonialWidth;
@@ -28,7 +29,7 @@ rightButton.addEventListener('click', () => {
         row.style.transform = `translateX(${currentPosition}px)`;
     }
 });
-
+// Отлик кнопки лайка
 likeButtons.forEach(button => {
     button.addEventListener('click', () => {
         button.classList.toggle('inverted');
